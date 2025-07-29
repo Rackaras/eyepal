3d688b386a6f3f9d7e0e46efdd99d9434cd165f7
Raul Cedeño <raulcm55@gmail.com>
2025-07-28 20:01:47 -0500

    chore: add swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,26 @@
 import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
 import { usersController } from "./modules/users";
 
 const app = new Elysia({
     prefix: "/api/v1",
     normalize: true,
-})
-    .use(usersController)
-    .listen(process.env.PORT || 3000);
+}).use(usersController);
+
+if (process.env.NODE_ENV === "development") {
+    app.use(
+        swagger({
+            documentation: {
+                info: {
+                    title: "Eyepal API",
+                    version: "1.0.0",
+                },
+            },
+            path: "/swagger",
+        })
+    );
+}
+
+app.listen(process.env.PORT || 3000);
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
